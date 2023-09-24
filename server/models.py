5713ebf88b0edaba19a8fb4ec1e739a3347acf5c
James Hawk <jph94880@gmail.com>
2023-09-24 16:15:35 -0500

    Creates users table schema

--- server/models.py
+++ server/models.py
@@ -5,10 +5,16 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
     pass
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key=True)
     
     pass
