301828622f6784bafdcd9fc1b8eebd9b8b7f9ff9
James Hawk <jph94880@gmail.com>
2023-09-26 21:12:07 -0500

    Passes 14 tests

--- server/app.py
+++ server/app.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-from flask import make_response, request, session
+from flask import request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,15 +6,24 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
+    
     def post(self):
-        username = request.get_json()['username']
-        password = request.get_json()['password']
-        image_url = request.get_json()['image_url']
-        bio = request.get_json()['bio']
+
+        request_json = request.get_json()
+
+        username = request_json.get('username')
+        password = request_json.get('password')
+        image_url = request_json.get('image_url')
+        bio = request_json.get('bio')
+
         user = User(
-            username = username, 
-            image_url = image_url, 
-            bio = bio)
+            username=username,
+            image_url=image_url,
+            bio=bio
+        )
+
+        # the setter will encrypt this
+        user.password_hash = password
 
         print('first')
 
@@ -38,62 +45,86 @@ class Signup(Resource):
             print('no, here!')
             
             return {'error': '422 Unprocessable Entity'}, 422
-        
+
 class CheckSession(Resource):
+    
     def get(self):
+
         if session.get('user_id'):
-            user = User.query.filter(User.id == session['user.id']).first()
-            return user.to_dict, 200
+
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return user.to_dict(), 200
+
         return {'error': '401 Unauthorized'}, 401
 
 class Login(Resource):
+    
     def post(self):
+
         request_json = request.get_json()
+
         username = request_json.get('username')
         password = request_json.get('password')
 
         user = User.query.filter(User.username == username).first()
 
-        if user: 
+        if user:
             if user.authenticate(password):
+
                 session['user_id'] = user.id
                 return user.to_dict(), 200
+
         return {'error': '401 Unauthorized'}, 401
 
-
 class Logout(Resource):
+    
     def delete(self):
+        
         if session.get('user_id'):
+            
             session['user_id'] = None
+            
             return {}, 204
+        
         return {'error': '401 Unauthorized'}, 401
 
 class RecipeIndex(Resource):
-    def get(self): 
-        if session.get(';user_id'):
-            user = User.query.filter(User.id == session['user_id']).first()
-            return [recipe.to_dict for recipe in user.recipes], 200
-        return {'error': '401 Unauthorized'}, 401
-    def post(self):
-        if session.get('user_id'):
-            request_json = request.get_json()
-            title = request_json['title']
-            instructions = request_json['title']
-            minutes_to_complete = request_json[',inutes_to_complete']
 
-            try: 
+    def get(self):
+
+        if session.get('user_id'):
+
+            user = User.query.filter(User.id == session['user_id']).first()
+
+            return [recipe.to_dict() for recipe in user.recipes], 200
+        
+        return {'error': '401 Unauthorized'}, 401
+        
+    def post(self):
+
+        if session.get('user_id'):
+
+            request_json = request.get_json()
+
+            title = request_json['title']
+            instructions = request_json['instructions']
+            minutes_to_complete = request_json['minutes_to_complete']
+
+            try:
 
                 recipe = Recipe(
                     title=title,
                     instructions=instructions,
                     minutes_to_complete=minutes_to_complete,
-                    user_id=session['user_id']
+                    user_id=session['user_id'],
                 )
+
                 db.session.add(recipe)
                 db.session.commit()
 
                 return recipe.to_dict(), 201
-            
+
             except IntegrityError:
 
                 return {'error': '422 Unprocessable Entity'}, 422
--- server/models.py
+++ server/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.orm import validates
 
 from config import db, bcrypt
 
@@ -8,7 +7,6 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     serialize_rules = ('-recipes.user', '-_password_hash',)
-                       
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
@@ -18,16 +16,10 @@ class User(db.Model, SerializerMixin):
 
     recipes = db.relationship('Recipe', backref='user')
 
-
-
-    # [x] incorporate bcrypt to create a secure password. Attempts to access the password_hash should be met with an AttributeError.
-    # [x] validate the user's username to ensure that it is present and unique (no two users can have the same username).
-    # [x] have many recipes.
-
     @hybrid_property
     def password_hash(self):
-        raise Exception('Password hashes may not be viewed.')
-    
+        raise AttributeError('Password hashes may not be viewed.')
+
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(
@@ -39,33 +31,20 @@ class User(db.Model, SerializerMixin):
             self._password_hash, password.encode('utf-8'))
 
     def __repr__(self):
-        return f'User {self.username}, ID: {self.id}'
+        return f'<User {self.username}>'
 
-
-
-# Add validations for the Recipe model:
-
-# [x] Title must be present.
-# [] Instructions must be present and at least 50 characters long.]
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     __table_args__ = (
         db.CheckConstraint('length(instructions) >= 50'),
     )
 
-    # A recipe belongs to a user. 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     instructions = db.Column(db.String, nullable=False)
     minutes_to_complete = db.Column(db.Integer)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
 
-    # users = db.relationship('User', backref='recipes')
-
-    # Issue is not with validates
-    # @validates('instructions')
-    # def validate_instructions_length(self, key, value):
-    #     if len(value) < 50:
-    #         raise ValueError("instructions must be present and at least 50 characters long")
-    #     return value
+    def __repr__(self):
+        return f'<Recipe {self.id}: {self.title}>'
